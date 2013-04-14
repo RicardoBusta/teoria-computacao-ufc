7c50dd9419f9d8b278000bc21357354b69066833
ricardobqueiroz@gmail.com <ricardobqueiroz@gmail.com@8dd267ad-68c7-0b83-f3b8-fc1b661d6993>
2013-04-14 04:27:33 +0000

    algumas mudanças na regexp. ainda não resolveu o validador do programa inteiro por causa das quebras de linha.

--- TuringMachine/mainwindow.cpp
+++ TuringMachine/mainwindow.cpp
@@ -12,8 +12,9 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 
     TMSyntax *syntax = new TMSyntax(ui->textEdit);
+    ex_valid_program = syntax->get_valid_exp();
 
-    connect(ui->pushButton, SIGNAL(clicked()), this, SLOT(compile()));
+    connect(ui->textEdit, SIGNAL(textChanged()), this, SLOT(compile()));
 }
 
 MainWindow::~MainWindow()
@@ -23,11 +24,13 @@ MainWindow::~MainWindow()
 
 void MainWindow::compile()
 {
+    qDebug() << ex_valid_program.pattern();
+    qDebug() << ui->textEdit->toPlainText();
     if (ex_valid_program.exactMatch(ui->textEdit->toPlainText())) {
-        qDebug() << "valid";
-        qDebug() << ex_valid_program.matchedLength();
+        ui->label->setText("valid");
+        ui->label->setStyleSheet("color: #0f0");
     }else{
-        qDebug() <<"invalid";
-        qDebug() << ex_valid_program.matchedLength();
+        ui->label->setText("invalid");
+        ui->label->setStyleSheet("color: #f00");
     }
 }
